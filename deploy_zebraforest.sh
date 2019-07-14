a3016b067e3d9b7601c2d864ac5b946a9e10b2be
xscrat <xjfscrat@gmail.com>
2019-07-14 09:13:55 +0800

    Add something to deploy_zebraforest.sh

--- deploy_zebraforest.sh
+++ deploy_zebraforest.sh
@@ -2,6 +2,8 @@
 sudo apt-get install qt5-default
 sudo apt-get install qt5-qmake
 sudo apt-get install qtbase5-dev
+sudo apt-get install libqt5webkit5
+sudo apt-get install libqt5webkit5-dev
 qmake
 make -j4
 ./ZebraForest
